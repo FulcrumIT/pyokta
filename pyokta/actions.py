961ae35f66410808274d5225e68d0a4fc0167719
calebh93 <hawkins.caleb93@gmail.com>
2016-05-02 11:58:13 -0700

    Added group functions

--- pyokta/actions.py
+++ pyokta/actions.py
@@ -14,3 +14,28 @@ def get_user(id):
     - id: id of an Okta user.
     """
     return _api.rest("/users/" + str(id))
+
+
+def get_all_groups():
+    """Return all Okta groups."""
+    return _api.rest("/groups")
+
+
+def get_group(id):
+    """Return data on one particular group.
+
+    Parameters:
+    - id: id of an Okta group.
+    """
+
+    return _api.rest("/groups/" + str(id))
+
+
+def get_group_users(id):
+    """Return users in a particular group.
+
+    Parameters:
+    - id: id of an Okta group.
+    """
+
+    return _api.rest("/groups/" + str(id) + "/users")
